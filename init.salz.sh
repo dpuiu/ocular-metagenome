bfc41ee7b5dba59d01ae79f0ba93f88da20b78a7
PuertoRicanGenome <dpuiu1@jhmi.edu>
2024-04-10 15:45:41 -0400

    .

--- init.salz.sh
+++ init.salz.sh
@@ -2,4 +2,4 @@ export REF1=/ccb/salz1/dpuiu/Homo_sapiens/T2T/bowtie2/chm13v2.0
 export REF2=/ccb/salz1/dpuiu/Homo_sapiens/hs38DH/bowtie2/hs38DH
 export KRAKENDB=/ccb/salz1/dpuiu/Homo_sapiens/krakendb-2023-08-08-MICROBIAL/
 export P=8
-export FLAGS="-f"
+export FLAGS=""  # "-f" for fasta
