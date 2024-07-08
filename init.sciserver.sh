7c7f86e711b64ea3fe69bdf7c042c353b445d4be
PuertoRicanGenome <dpuiu1@jhmi.edu>
2024-07-08 12:30:34 -0400

    .

--- init.sciserver.sh
+++ init.sciserver.sh
@@ -2,6 +2,7 @@ export PATH=$PATH:/home/idies/workspace/OcularMicrobiome/sw/bin/:/home/idies/wor
 
 export REF1=/home/idies/workspace/OcularMicrobiome/Ref/chm13v2.0.mmi
 export REF2=/home/idies/workspace/OcularMicrobiome/Ref/hs38DH.mmi
-export KRAKENDB=/home/idies/workspace/OcularMicrobiome/krakendbs/krakendb-2023-08-08-MICROBIAL/
+export KRAKENDB=/home/idies/workspace/OcularMicrobiome/krakendbs/MicrobialDB_202308/
 export BRAKENDB=$KRAKENDB/krakenuniq_db_MICROBIAL_20230808/
 export P=8
+
